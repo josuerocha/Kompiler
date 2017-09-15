0454da4c0d45edbe0f49273b041d6f8e33158ec6
josuerocha <josuerocha@me.com>
2017-09-15 12:08:57 -0300

    Registering tokens.

--- src/dataunits/Tag.java
+++ src/dataunits/Tag.java
@@ -1,26 +1,4 @@
-/*
- * The MIT License
- *
- * Copyright 2017 jr.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
+
 package dataunits;
 
 /**
@@ -36,30 +14,31 @@ public enum Tag {
  * **/
     
     //RESERVED WORDS
-    FOR(256), 
-    WHILE(257), 
-    STATIC(258), 
-    PRIVATE(259),
-    PUBLIC(260),
+    PROGRAM(256),
+    IF(257),
+    THEN(258),
+    ELSE(259),
+    END(260),
+    DO(261),
+    WHILE(262),
+    SCAN(263),
+    PRINT(264),
+    
+    //DATATYPES
+    INT(300),
+    STRING(301),
     
     //RELATIONAL OPERATORS
-    LESS(300), 
-    GREATER(301), 
-    LESS_EQUAL(302), 
-    GREATER_EQUAL(303),
+    REL_OP(350),
     
     //MATHEMATICAL OPERATORS
-    ASSIGN(350), 
-    PLUS(351), 
-    MINUS(352), 
-    TIMES(353), 
-    DIVIDE(354),
-    
-    
+    MATH_OP(400),
     
     //OTHER
     CONST(400),
-    IDENTIFIER(401);
+    IDENTIFIER(401),
+    SEMI_COLLON(402),
+    PARENTHESIS(403);
     
     /**
     * Tag value container.
