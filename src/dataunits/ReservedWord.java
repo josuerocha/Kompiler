78b1d88830d3296c5dd2480826da2fe851c7ba5e
josuerocha <josuerocha@me.com>
2017-09-16 21:04:15 -0300

    Adding reserved word list.

--- src/dataunits/ReservedWord.java
+++ src/dataunits/ReservedWord.java
@@ -8,15 +8,20 @@ package dataunits;
 
 public class ReservedWord extends Token{
     public static final int
-    PROGRAM = 256,
-    IF = 257,
-    THEN = 258,
-    ELSE = 259,
-    END = 260,
-    DO = 261,
-    WHILE = 262,
-    SCAN = 263,
-    PRINT = 264;
+    PROGRAM_ID = 256,
+    IF_ID = 257,
+    THEN_ID = 258,
+    ELSE_ID = 259,
+    END_ID = 260,
+    DO_ID = 261,
+    WHILE_ID = 262,
+    SCAN_ID = 263,
+    PRINT_ID = 264;
+    
+    public static ReservedWord PROGRAM = new ReservedWord("program");
+    public static ReservedWord IF = new ReservedWord("if");
+    public static ReservedWord THEN = new ReservedWord("then");
+    public static ReservedWord ELSE = new ReservedWord("else");
     
     private int id;
     private String lexeme;
@@ -26,11 +31,39 @@ public class ReservedWord extends Token{
         
         this.lexeme = lexeme;
         
-        
+        switch(lexeme){
+            case "program":
+                this.id = ReservedWord.PROGRAM_ID;
+            break;
+            case "if":
+                this.id = ReservedWord.IF_ID;
+            break;
+            case "then":
+                this.id = ReservedWord.THEN_ID;
+            break;
+            case "else":
+                this.id = ReservedWord.ELSE_ID;
+            case "end":
+                this.id = ReservedWord.END_ID;
+            case "do":
+                this.id = ReservedWord.DO_ID;
+            case "while":
+                this.id = ReservedWord.WHILE_ID;
+            case "scan":
+                this.id = ReservedWord.SCAN_ID;
+            case "print":
+                this.id = ReservedWord.PRINT_ID;
+        }
         
     }
     
+    
+
+    public String getLexeme() {
+        return lexeme;
+    }
+    
     public String toString(){
-        return "< " + lexeme + " >";
+        return "< " + lexeme.toUpperCase() + " >";
     }
 }
