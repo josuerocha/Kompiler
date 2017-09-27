038f66628c620e6fad6213873f454c57739baf9a
josuerocha <josuerocha@me.com>
2017-09-27 01:10:41 -0300

    [MAJOR] Multi-thread one-step parser implementation done.

--- src/modules/Parser.java
+++ src/modules/Parser.java
@@ -1,24 +1,67 @@
 package modules;
 
 import dataunits.Token;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  *
  * @author jr
  */
-public class Parser {
-
-    public static void main(String[] args) {
-
-        String path = "test/test1.j";
-        Lexer lexer = new Lexer(path);
+public class Parser extends Thread {
+    
+    private static final int MAX_JOBS = 10;
+    private String path;
+    private StringBuffer output;
+    
+    public Parser(String path){
+        this.path = path;
+        output = new StringBuffer();
+    }
+    
+    public void run(){
+        Lexer lexer = new Lexer(this.path);
 
         Token token;
         while ((token = lexer.getToken()) != null) {
 
-            System.out.print(token + "\n");
+            output.append(token + "\n");
+                    
         }
-
+        
+        printTokens();
+    }
+    
+    public synchronized void printTokens(){
+        System.out.println("FILE: " + this.path);
+        System.out.print(this.output.toString());
+        System.out.println("__________________________");
+    }
+    
+    public static void main(String[] args) {
+        List<String> paths = new ArrayList<String>();
+        List<Parser> compileJobs = new ArrayList<Parser>();
+        
+        paths.addAll(Arrays.asList(args));
+        
+        
+        while(paths.size() > 0){
+            
+            if(compileJobs.size() < MAX_JOBS){
+                compileJobs.add(new Parser(paths.remove(0)));
+                compileJobs.get(compileJobs.size() -1).start();
+            }
+            
+            for(int i=0 ; i<compileJobs.size(); i++){
+                if(!compileJobs.get(i).isAlive()){
+                    compileJobs.remove(i);
+                }
+            }
+            
+            
+        }
+       
     }
 
 }
