e9552abe1171480ccb7074c027424b86162d0795
josuerocha <josuerocha@me.com>
2017-11-03 15:23:41 -0200

    [MAJOR] Implementing production rules. .

--- src/modules/Parser.java
+++ src/modules/Parser.java
@@ -2,6 +2,9 @@ package modules;
 
 import dataunits.CompileError;
 import dataunits.Identifier;
+import dataunits.IntConstant;
+import dataunits.LiteralConstant;
+import dataunits.Operator;
 import dataunits.ReservedWord;
 import dataunits.Token;
 import java.util.ArrayList;
@@ -51,58 +54,239 @@ public class Parser extends Thread {
     }
     
     private void program(){
-        eat(ReservedWord.PROGRAM); declList(); stmtList(); eat(ReservedWord.END);
+        switch(currentToken.getTag()){
+            case ReservedWord.PROGRAM_ID:
+                eat(ReservedWord.PROGRAM); declList(); stmtList(); eat(ReservedWord.END);
+                break;
+            default:
+                System.out.println("PROGRAM initializer expected.");
+                break;
+        }
     }
     
     private void declList(){
-        declaration() ;
+        switch(currentToken.getTag()){
+            case Token.IDENTIFIER:
+                declListPrime();
+                break;
+            case ReservedWord.IF_ID:
+            case ReservedWord.DO_ID:
+            case ReservedWord.SCAN_ID:
+            case ReservedWord.PRINT_ID:
+                
+                break;
+                
+            default:
+                System.out.println("Expected IDENTIFIER");
+        }
+    }
+    
+    private void stmtList(){
+        switch(currentToken.getTag()){
+            case Token.IDENTIFIER:
+            case ReservedWord.DO_ID:
+            case ReservedWord.PRINT_ID:
+            case ReservedWord.SCAN_ID:
+            case ReservedWord.IF_ID:
+                stmtListPrime();
+                break;
+                
+            case ReservedWord.WHILE_ID:
+            case ReservedWord.ELSE_ID:
+            case ReservedWord.END_ID:
+                
+                break;
+                
+            default:
+                System.out.println("Syntax error on stmtList");
+        } 
+     
+    }
+    
+    private void stmtListPrime(){
+        switch(currentToken.getTag()){
+            case Token.IDENTIFIER:
+            case ReservedWord.DO_ID:
+            case ReservedWord.PRINT_ID:
+            case ReservedWord.IF_ID:
+            case ReservedWord.SCAN_ID:
+                stmt(); stmtList();
+                break;
+                
+            default:
+                System.out.println("Expected statement");
+        }
+    }
+    
+    private void declListPrime(){
+        switch(currentToken.getTag()){
+            case Token.IDENTIFIER:
+                decl();
+            default:
+                System.out.println("ERROR: expected ID");
+        }
+    }
+    
+    private void decl(){
+        switch(currentToken.getTag()){
+            case Token.IDENTIFIER:
+                eat(new Identifier()); possibleIdentifier();
+            default:
+                System.out.println("ERROR: expected ID");
+        }
+    }
+    
+    private void possibleIdentifier(){
+        switch(currentToken.getTag()){
+            case ',':
+                eat(Token.COMMA); decl();
+                break;
+            case Token.IDENTIFIER:
+            case ReservedWord.DO_ID:
+            case ReservedWord.PRINT_ID:
+            case ReservedWord.IF_ID:
+            case ReservedWord.SCAN_ID:
+                
+                break;
+            default:
+                System.out.println("ERROR: COMMA EXPECTED");
+        }
     }
     
     public void assignStatement(){
+        switch(currentToken.getTag()){
+            case Token.IDENTIFIER:
+                eat(new Identifier()); eat(new Operator('=')); simpleExpr();
+                break;
+            default:
+                System.out.println("ERROR: Expected IDENTIFIER");
+        }
+    }
+    
+    private void ifStatement(){
+        switch(currentToken.getTag()){
+            case ReservedWord.IF_ID:
+                eat(ReservedWord.IF); condition(); eat(ReservedWord.THEN); stmtListPrime(); ifStatementPrime();
+                break;
+            default:
+                System.out.println("ERROR: expected if");
+        }
+    }
+    
+    private void ifStatementPrime(){
+        switch(currentToken.getTag()){
+            case ReservedWord.END_ID:
+                eat(ReservedWord.END);
+            case ReservedWord.ELSE_ID:
+                eat(ReservedWord.ELSE); stmtList(); eat(ReservedWord.END);
+            default:
+                System.out.println("ERROR: Expected END or ELSE");
+        }
+    }
+    
+    private void simpleExpr(){
         
     }
     
-    public void ifStatement(){
-        eat(ReservedWord.IF); expression(); eat(ReservedWord.THEN); 
-        stmtList(); eat(ReservedWord.END);
+    private void whileStatement(){
+        switch(currentToken.getTag()){
+            case ReservedWord.DO_ID:
+                eat(ReservedWord.DO); stmtListPrime(); stmtSufix();
+                
+            default:
+                System.out.println("ERROR: expected do");
+        }
     }
     
-    public void whileStatement(){
-        eat(ReservedWord.WHILE); expression(); eat(ReservedWord.END);
+    private void stmtSufix(){
+        switch(currentToken.getTag()){
+            case ReservedWord.WHILE_ID:
+                eat(ReservedWord.WHILE); condition(); eat(ReservedWord.END);
+                break;
+            default:
+                System.out.println("ERROR: WHILE keyword expected");
+        }
     }
     
     public void readStatement(){
-        eat(ReservedWord.SCAN); eat(Token.OPEN_PAREN); 
-        eat(new Identifier()); eat(Token.CLOSE_PAREN);
+        switch(currentToken.getTag()){
+            case ReservedWord.SCAN_ID:
+                eat(ReservedWord.SCAN); eat(Token.OPEN_PAREN); 
+                eat(new Identifier()); eat(Token.CLOSE_PAREN);
+                break;
+            
+            default:
+                System.out.println("ERROR: expected SCAN operator");
+        }
+        
     }
     
     public void writeStatement(){
-        eat(ReservedWord.PRINT); eat(Token.OPEN_PAREN); writable(); eat(Token.CLOSE_PAREN);
+        switch(currentToken.getTag()){
+            case ReservedWord.PRINT_ID:
+                eat(ReservedWord.PRINT); eat(Token.OPEN_PAREN); writable(); eat(Token.CLOSE_PAREN);
+                break;
+                
+            default:
+                System.out.println("ERROR: expected PRINT keyword");
+        }
     }
     
     private void expression(){
-        
+        switch(currentToken.getTag()){
+            case ReservedWord.PRINT_ID:
+                
+        }
     }
     
     private void writable(){
         
     }
     
-    private void stmtList(){
-        stmt(); stmt();
-    }
-    
     private void declaration(){
-        type(); identList(); eat(new Token(';'));
-    }
-    
-    private void stmt(){
         
     }
     
-    private void identList(){
-        eat(new Identifier()); eat(new Token(',')) ; eat(new Token(Identifier.IDENTIFIER));
+    private void condition(){
+        switch(currentToken.getTag()){
+            case '!':
+            case '-':
+            case Token.IDENTIFIER:
+            case '(':
+            case IntConstant.INT_CONSTANT:
+            case LiteralConstant.LITERAL_CONSTANT:
+                expression();
+                break;
+                
+            default:
+                System.out.println("Error in condition production");
+        }
     }
+    
+    private void stmt(){
+        switch(currentToken.getTag()){
+            case Token.IDENTIFIER:
+                    assignStatement(); eat(Token.SEMI_COLON);
+                break;
+                
+            case ReservedWord.DO_ID:
+                whileStatement();
+                break;
+                
+            case ReservedWord.PRINT_ID:
+                writeStatement(); eat(Token.SEMI_COLON);
+                break;
+            case ReservedWord.SCAN_ID:
+                readStatement(); eat(Token.SEMI_COLON);
+                break;
+            case ReservedWord.IF_ID:
+                ifStatement();
+                break;
+            default:
+                System.out.println("ERROR: expected statement");
+        }
+    }
+    
     
     private void type(){
         switch (currentToken.getTag()){
@@ -115,7 +299,7 @@ public class Parser extends Thread {
                 break;
                 
             default:
-                    
+                    System.out.println("ERROR: type expected (INT/STRING)");
                 break;
                 
         }
