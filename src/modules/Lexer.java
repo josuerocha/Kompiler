3b72a9c67251d4d6340c37401f2cb9bf9230398e
josuerocha <josuerocha@me.com>
2017-12-08 23:52:59 -0200

    [FINAL] Adding report.

--- src/modules/Lexer.java
+++ src/modules/Lexer.java
@@ -10,7 +10,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.regex.Pattern;
 import util.PrintColor;
 
